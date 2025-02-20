dd42803392b81d28f9429218887f146760f88cfd
Jose Viosca Ros <jviosca@gmail.com>
2025-02-20 20:52:54 +0100

    ajustar margen a ambos lados del mapa para mejorar visualizacion en dispositivos moviles

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ if page == "📍 Mapa":
             ).add_to(m)
 
     # Mostrar el mapa en Streamlit
-    col1, col2, col3 = st.columns([0.25,0.5,0.25])
+    col1, col2, col3 = st.columns([0.1,0.8,0.1])
     with col2:
         st_folium(m, width='100%')
 
